58f912dac92739068e8dee41e7f8867537e408c4
tsuzukidaigo <daigo03031995@gmail.com>
2020-12-05 19:02:57 +0900

    width=100%

--- src/components/card.js
+++ src/components/card.js
@@ -2,10 +2,8 @@ import React from "react"
 import { makeStyles } from "@material-ui/core/styles"
 import Card from "@material-ui/core/Card"
 import CardActionArea from "@material-ui/core/CardActionArea"
-import CardActions from "@material-ui/core/CardActions"
 import CardContent from "@material-ui/core/CardContent"
 import CardMedia from "@material-ui/core/CardMedia"
-import Button from "@material-ui/core/Button"
 import Typography from "@material-ui/core/Typography"
 import { Link } from "gatsby"
 
@@ -29,7 +27,8 @@ const BlogCard = props => {
           <CardMedia
             component="img"
             alt={title}
-            height="250"
+            height="250px"
+            width="100%"
             image={eyecatch.file.url}
             title={title}
           />
