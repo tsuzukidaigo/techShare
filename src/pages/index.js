f25d8318e7964c83f32c177ad1bb6aa44fba3e15
tsuzukidaigo <daigo03031995@gmail.com>
2020-12-05 18:40:43 +0900

    元に戻した

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,7 +57,7 @@ const IndexPage = props => {
         <SEO title="Home" />
         <div className="container">
           <h1 className={classes.title}>全ての記事</h1>
-          <div>
+          <div className="card">
             {data.allContentfulBlogPost.edges.map(({ node }, index) => (
               <BlogCard
                 title={node.title}
